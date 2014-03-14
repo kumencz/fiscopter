9dd4973e02368d05959137b7323a9749d598091d
Jakub Skalicky <jakub.skalicky@rieter.com>
2014-03-14 11:04:26 +0100

    Complete rx parser

---
+++ FlyControler/FlyControler/RxMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FlyControler
+{
+    public class ParseMessgaeArgs : EventArgs
+    {
+        public RxMessages Msg_type { private set; get; }
+        public object Msg_data {private set; get;}
+
+        public ParseMessgaeArgs(RxMessages type, object data)
+        {
+            this.Msg_type = type;
+            this.Msg_data = data;
+        }
+    }
+
+    enum RxMessages
+    {
+        UNDEFINED,
+        K_CHECK_LIVE = "K_CHECK_LIVE",
+        K_RPM_SET = "K_RPM_SET"
+    }
+}
--- FlyControler/FlyControler/RxParser.cs
+++ FlyControler/FlyControler/RxParser.cs
@@ -8,6 +8,8 @@ using Tamir.SharpSsh;
 
 namespace FlyControler
 {
+
+
     class RxParser
     {
         private List<string> RxBufferList = new List<string>();
@@ -16,6 +18,7 @@ namespace FlyControler
         byte[] rx_buff = new byte[200];
         SshStream ssh_rx;
         AsyncCallback rx_clbk;
+        public event EventHandler<ParseMessgaeArgs> RxMessageReceived_event;
 
         public RxParser()
         {
@@ -62,15 +65,24 @@ namespace FlyControler
 
         private void Parse()
         {
+            RxMessages parsed_msg;
+            object parsed_data = null;
             if (RxBufferList.Count > 0)
             {
                 if (String.Compare(RxBufferList[0], "LIVE_OK\n") == 0)
                 {
+                    parsed_msg = RxMessages.K_CHECK_LIVE;
                 }
                 else if (String.Compare(RxBufferList[0], "NejakA_dalsi_zprava\n") == 0)
                 {
+                    parsed_msg = RxMessages.K_RPM_SET;
+                }
+                else
+                {
+                    parsed_msg = RxMessages.UNDEFINED;
                 }
                 this.RxBufferList.RemoveAt(0);
+                if (this.RxMessageReceived_event != null) RxMessageReceived_event(this, new ParseMessgaeArgs(parsed_msg, parsed_data));
             }
         }
 
